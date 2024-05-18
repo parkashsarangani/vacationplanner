ca7d07a4d59c03dc6340bd25008f66de383788a4
Parkash <parkashsarangani@hotmail.com>
2024-05-18 11:43:57 +0200

    Initial push

---
+++ src/main/java/com/ai/vacationplanner/VacationPlannerAiApplication.java
@@ -0,0 +1,13 @@
+package com.ai.vacationplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationPlannerAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationPlannerAiApplication.class, args);
+	}
+
+}
